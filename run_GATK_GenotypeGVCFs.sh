d5081bafc17b86a1b72073fd03c45e34d7f3763a
David McGaughey <mcgaughey@gmail.com>
2015-12-15 14:11:24 -0500

    Added GATK combinevariants run to merge together indel and SNP vcf files

--- run_GATK_GenotypeGVCFs.sh
+++ run_GATK_GenotypeGVCFs.sh
@@ -30,23 +30,24 @@ output_vcf_name=$2
 #	-o ${2%.vcf}.rawINDEL.vcf
 
 # Hard filters on GATK best practices for SNPs with MQ mod from bcbio
-GATK -m 8g VariantFiltration \
-	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
-	-V ${2%.vcf}.rawSNP.vcf \
-	--filterExpression "QD < 2.0 || FS > 60.0 || MQ < 30.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
-	--filterName "FAIL_McGaughey_SNP_filter_v01" \
-	-o ${2%.vcf}.filterSNP.vcf
+#GATK -m 8g VariantFiltration \
+#	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+#	-V ${2%.vcf}.rawSNP.vcf \
+#	--filterExpression "QD < 2.0 || FS > 60.0 || MQ < 30.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
+#	--filterName "FAIL_McGaughey_SNP_filter_v01" \
+#	-o ${2%.vcf}.filterSNP.vcf
 
 # Hard filters on GATK bests for INDELs
-GATK -m 8g VariantFiltration \
-	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
-	-V ${2%.vcf}.rawINDEL.vcf \
-	--filterExpression "QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0" \
-	--filterName "FAIL_McGaughey_INDEL_filter_v01" \
-	-o ${2%.vcf}.filterINDEL.vcf
+#GATK -m 8g VariantFiltration \
+#	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+#	-V ${2%.vcf}.rawINDEL.vcf \
+#	--filterExpression "QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0" \
+#	--filterName "FAIL_McGaughey_INDEL_filter_v01" \
+#	-o ${2%.vcf}.filterINDEL.vcf
 	
-GATK -m 8g CombineVariants
+GATK -m 8g CombineVariants \
 	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
 	--variant ${2%.vcf}.filterSNP.vcf \
 	--variant ${2%.vcf}.filterINDEL.vcf \
-	-o ${2%.vcf}.filterSNP-INDEL.vcf
+	-o ${2%.vcf}.filterSNP-INDEL.vcf \
+	--genotypemergeoption UNSORTED

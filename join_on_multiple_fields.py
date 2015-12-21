1e08a2b4ff7806e3f5097445e3ecacc009ba10ec
David McGaughey <mcgaughey@gmail.com>
2015-12-21 17:08:19 -0500

    Lightweight script that takes two files and a comma separated list of column numbers to join on. Will build a dictionary to hold first file, so can get in trouble with giant files with memory usage. Needs some more polish on helpful error prompts

--- join_on_multiple_fields.py
+++ join_on_multiple_fields.py
@@ -10,6 +10,7 @@ import sys
 file1 = open(sys.argv[1])
 file2 = open(sys.argv[2])
 columns = str(sys.argv[3])
+print_second_file = str(sys.argv[4]) 
 
 def key_columns(columns_input):
 	cols = columns_input.split(',')
@@ -23,25 +24,27 @@ def build_key(col_list, sline):
 
 def build_dict(file, col_list):
 	file1_dict = {}
-	for line in file.readline():
+	for line in file:
 		sline = line.split('\t')
 		key = build_key(col_list, sline)
 		if key in file1_dict:
 			print("Non-unique key in first file")
 			break
 		else:
-			file1_dict[key] = line
+			file1_dict[key] = line[:-1]
 	return(file1_dict)
 
 	
 def main():
 	col_list = key_columns(columns)
 	file1_dict = build_dict(file1, col_list)
-	for line in file2.readline():
+	for line in file2:
 		sline = line.split('\t')
 		key = build_key(col_list, sline)	
 		if key in file1_dict:
-			print(file1_dict[key], line, sep="\t")
-		
+			if print_second_file == 'n':
+				print(file1_dict[key], sep="\t")
+			else:
+				print(file1_dict[key], line[:-1], sep="\t")
 
 main()

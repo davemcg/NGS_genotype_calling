47cc770a704e0140ee35ede43f2437d3887e6d7b
David McGaughey <mcgaughey@gmail.com>
2016-01-12 15:03:56 -0500

    Now adds ExAC frequencies. I think

--- run_VEP82.sh
+++ run_VEP82.sh
@@ -5,14 +5,15 @@ module load VEP/82
 
 input_vcf=$1
 genome=$2
-
+cores=$3
 
 if [ "$genome" == "GRCh38" ] || [ "$genome" == "GRCh37" ]; then
 	variant_effect_predictor.pl -i $input_vcf --offline \
 	--cache --dir_cache $VEPCACHEDIR \
 	--fasta $VEPCACHEDIR/human.fa --species human --assembly $genome  \
 	--output ${input_vcf%.vcf}.VEP82everything.$genome.vcf \
-	--everything --vcf --force_overwrite --fork 10
+	--plugin ExAC,/data/mcgaugheyd/genomes/hg19/ExAC.r0.3.nonTCGA.sites.vep.vcf.gz,AC \
+	--everything --vcf --force_overwrite --fork $cores
 else
     echo "Pick either GRCh38 or GRCh37 genomes"
 fi

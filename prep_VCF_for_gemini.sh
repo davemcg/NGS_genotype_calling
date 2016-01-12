4e9adac7313c9ed75abdd67ac77198acf679b1f8
David McGaughey <mcgaughey@gmail.com>
2016-01-12 16:55:16 -0500

    Fixed typos and bugs. run_VEP82 fasta explictly set to correct build now

--- prep_VCF_for_gemini.sh
+++ prep_VCF_for_gemini.sh
@@ -6,15 +6,17 @@
 
 VCF=$1
 cores=$2
-
+mkdir tmp
 cat $VCF \
 	| sed 's/ID=AD,Number=./ID=AD,Number=R/' \
 	| ~/git/vt/./vt decompose -s - \
 	| ~/git/vt/./vt normalize -r /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta - \
-	> /tmp/$VCF 
+	> tmp/$VCF 
 
-/home/mcgaugheyd/bin/run_VEP82.sh /tmp/$VCF GRCh37 $cores
+/home/mcgaugheyd/bin/run_VEP82.sh tmp/$VCF GRCh37 $cores
 
 bgzip ${VCF%.vcf}.VEP82everything.GRCh37.vcf
 
 tabix -p vcf ${VCF%.vcf}.VEP82everything.GRCh37.vcf
+
+rm -rf tmp/*
--- run_VEP82.sh
+++ run_VEP82.sh
@@ -10,7 +10,7 @@ cores=$3
 if [ "$genome" == "GRCh38" ] || [ "$genome" == "GRCh37" ]; then
 	variant_effect_predictor.pl -i $input_vcf --offline \
 	--cache --dir_cache $VEPCACHEDIR \
-	--fasta $VEPCACHEDIR/human.fa --species human --assembly $genome  \
+	--fasta $VEPCACHEDIR/$genome.fa --species human --assembly $genome  \
 	--output ${input_vcf%.vcf}.VEP82everything.$genome.vcf \
 	--plugin ExAC,/data/mcgaugheyd/genomes/hg19/ExAC.r0.3.nonTCGA.sites.vep.vcf.gz,AC \
 	--everything --vcf --force_overwrite --fork $cores

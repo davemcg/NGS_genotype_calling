a06ea5366671b48ceb6695666b769aa52b76ef46
David McGaughey <mcgaughey@gmail.com>
2016-01-12 15:02:57 -0500

    Script to modify GATK vcf with vt and annotate with VEP for use in gemini

--- prep_VCF_for_gemini.sh
+++ prep_VCF_for_gemini.sh
@@ -2,17 +2,19 @@
 
 # Assumes a GATK processed VCF after GATK-recommended filtering
 # Hard coded against hg19/grch37
+# 
 
 VCF=$1
+cores=$2
 
 cat $VCF \
 	| sed 's/ID=AD,Number=./ID=AD,Number=R/' \
 	| ~/git/vt/./vt decompose -s - \
 	| ~/git/vt/./vt normalize -r /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta - \
-	> test.vcf
+	> /tmp/$VCF 
 
-/home/mcgaugheyd/bin/run_VEP82.sh test.vcf GRCh37
+/home/mcgaugheyd/bin/run_VEP82.sh /tmp/$VCF GRCh37 $cores
 
-bgzip test.VEP82everything.GRCh37.vcf
+bgzip ${VCF%.vcf}.VEP82everything.GRCh37.vcf
 
-tabix -p vcf test.VEP82everything.GRCh37.vcf.gz
+tabix -p vcf ${VCF%.vcf}.VEP82everything.GRCh37.vcf

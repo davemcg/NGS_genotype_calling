9ed80348c1cce4bed2ab7066ec71194650044c17
David McGaughey <mcgaughey@gmail.com>
2016-01-14 12:37:18 -0500

    Trying out some alternative text to change the output to better suit gemini

--- run_VEP82.sh
+++ run_VEP82.sh
@@ -14,6 +14,8 @@ if [ "$genome" == "GRCh38" ] || [ "$genome" == "GRCh37" ]; then
 	--output ${input_vcf%.vcf}.VEP82everything.$genome.vcf \
 	--plugin ExAC,/data/mcgaugheyd/genomes/hg19/ExAC.r0.3.nonTCGA.sites.vep.vcf.gz,AC \
 	--plugin Grantham \
+	--total_length \
+	--fields Consequence, Codons, Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE,CANONICAL,CADD_raw,CADD_phred,Grantham
 	--everything --vcf --force_overwrite --fork $cores
 else
     echo "Pick either GRCh38 or GRCh37 genomes"

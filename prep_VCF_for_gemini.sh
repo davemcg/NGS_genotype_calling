3339e3928730f4183111eda84767ea07077d8105
David McGaughey <mcgaughey@gmail.com>
2016-01-14 10:28:03 -0500

    Now filters with vcfffilter on AF. Only keeps variants with AF > 0.25

--- prep_VCF_for_gemini.sh
+++ prep_VCF_for_gemini.sh
@@ -3,20 +3,30 @@
 # Assumes a GATK processed VCF after GATK-recommended filtering
 # Hard coded against hg19/grch37
 # 
+module load vcftools
 
 VCF=$1
 cores=$2
 mkdir tmp
-cat $VCF \
+
+#only keep AF > 0.25
+vcffilter -f "AF > 0.25" $VCF > tmp/$VCF.AFfiltered
+
+# vt to "left align and trim alternative variants"
+cat tmp/$VCF.AFfiltered \
 	| sed 's/ID=AD,Number=./ID=AD,Number=R/' \
 	| ~/git/vt/./vt decompose -s - \
 	| ~/git/vt/./vt normalize -r /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta - \
 	> tmp/$VCF 
 
+# annotate with VEP82
 /home/mcgaugheyd/bin/run_VEP82.sh tmp/$VCF GRCh37 $cores
 
+# move out of tmp folder
+mv tmp/${VCF%.vcf}.VEP82everything.GRCh37.vcf* . 
+
+# compress and index
 bgzip ${VCF%.vcf}.VEP82everything.GRCh37.vcf
+tabix -p vcf ${VCF%.vcf}.VEP82everything.GRCh37.vcf.gz
 
-tabix -p vcf ${VCF%.vcf}.VEP82everything.GRCh37.vcf
-
-rm -rf tmp/*
+rm -rf tmp

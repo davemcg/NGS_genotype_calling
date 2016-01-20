adbe7a47fad60255aec2aa0de026b8c1958f1459
David McGaughey <mcgaughey@gmail.com>
2016-01-20 15:30:53 -0500

    Fixed typos, got chaining jobs to work, updated name of realigner script

--- bwa-gatk_workflow_v1.sh
+++ bwa-gatk_workflow_v1.sh
@@ -16,19 +16,18 @@ nisc_input_bam=$1
 ############
 #RE-ALIGNMENT
 # pulls bwa-formatted info (read group info, bam file, etc) then hands over to sbatch 
-bwa_command=$(sbatch --dependency=afterok:$j1 ~/bin/sbatch_command_withRG_align_fastq_bare.py  $1 -o $1.temp)
-j2=$(sbatch --dependency=afterok:$bwa_command --mem=50g --cpus-per-task 10 $bwa_command)
+j1=$(sbatch --mem=50g --cpus-per-task 10 ~/bin/realign_NISC_bam_with_bwa.py $1)
 ############
 
 ############
 # BAM processing
 # sort, mark duplicates, and index bam
-j3=$(sbatch --dependency=afterok:$j2 --mem=20G ~/bin/run_Picard_SortMarkIndexBam.sh ${1%.bam}.bwa-mem.hg19.bam)
+j2=$(sbatch --dependency=afterok:$j1 --mem=20G ~/bin/run_Picard_SortMarkIndexBam.sh ${1%.bam}.bwa-mem.hg19.bam)
 ############
 
 ############
 # Call GVCF
 # Individual GVCF file, which in conjuction with other GVCFs and filtering (hard or VQSR) are used
 # to make a VCF file with called genotypes
-j4=$(sbatch --dependency=afterok:$j3 --mem=16g --time=6:00:00 ~/bin/run_GATK_callGVCFs.sh ${1%.bam}.bwa-mem.hg19.sorted.markDup.bam)
+j3=$(sbatch --dependency=afterok:$j2 --mem=16g --time=6:00:00 ~/bin/run_GATK_callGVCFs.sh ${1%.bam}.bwa-mem.hg19.sorted.markDup.bam)
 ############

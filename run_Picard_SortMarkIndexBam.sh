b9b587dbeeaaeeeb390431c9d929d16e3e6ce9d2
David McGaughey <mcgaughey@gmail.com>
2016-01-20 09:23:05 -0500

    Screwed up which intermediate files to delete. Now fixed?

--- run_Picard_SortMarkIndexBam.sh
+++ run_Picard_SortMarkIndexBam.sh
@@ -4,6 +4,8 @@ module load picard
 
 # Sort bam, then marks duplicates, then creates index
 
+echo $1
+
 java -Xmx20g -jar $PICARDJARPATH/picard.jar SortSam \
 	INPUT=$1 OUTPUT=${1%.bam}.sorted.bam SORT_ORDER=coordinate
 
@@ -16,3 +18,8 @@ sorted_markDup_bam=${sorted_bam%.bam}.markDup.bam
 
 java -Xmx20g -jar $PICARDJARPATH/picard.jar BuildBamIndex \
 	INPUT=$sorted_markDup_bam OUTPUT=$sorted_markDup_bam.bai
+
+# Delete intermediate files
+rm $1
+rm ${1%.bam}.sorted.bam
+#rm ${sorted_bam%.bam}.markDup.bam

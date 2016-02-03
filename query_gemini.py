c11113e21ed0ba514e9c4dbab441cc805a69333f
David McGaughey <mcgaughey@gmail.com>
2016-02-03 11:56:22 -0500

    nothing yet

---
+++ query_gemini.py
@@ -0,0 +1,9 @@
+#!/usr/local/Anaconda/envs/py3.4.3/bin/python
+
+
+"""
+Runs autosomal_recessive, de_novo, mendel_errors, comp_hets, autosomal_dominant, and roh
+tests on select families (trios) in a gemini database
+
+Returns results in a xlsx file for the clinicians/genetic counselors
+"""

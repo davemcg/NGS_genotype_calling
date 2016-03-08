b5dc7a3d230de9493963f99472cae11165cb01b3
David McGaughey <mcgaughey@gmail.com>
2016-03-08 12:41:48 -0500

    Big change: Info worksheet with some stats (# of genotypes (hom_ref,het,hom_alt,total) by sample), ped file, gemini queries used) and a small change: changed cadd_raw for cadd_scaled

--- query_gemini.py
+++ query_gemini.py
@@ -44,7 +44,7 @@ def autosomal_recessive(db, family):
 					"--families " + family + " " + db + " " + filter
 	ar = subprocess.check_output(ar_query,shell=True).decode('utf-8')
 	ar = ar.split('\n')
-	return(ar)
+	return(ar,ar_query)
 
 def de_novo(db, family):
 	filter = " --filter \"max_aaf_all < 0.005 AND (is_coding=1 OR is_splicing=1) \
@@ -56,7 +56,7 @@ def de_novo(db, family):
 					"--families " + family + " " + db + " " + filter
 	dn = subprocess.check_output(dn_query,shell=True).decode('utf-8')	
 	dn = dn.split('\n')
-	return(dn)
+	return(dn, dn_query)
 
 def mendel_errors(db, family):
 	# gemini v0.18 has a bug with this call:
@@ -81,7 +81,7 @@ def mendel_errors(db, family):
 			s_line = line.split('\t')
 			if line and s_line[family_id_index]==family:
 				me_out.append(line)
-	return(me_out)
+	return(me_out, me_query)
 
 def comp_hets(db, family):
 	# can't call exac numbers in v0.18 (reported bug, fixed in next release)
@@ -99,7 +99,7 @@ def comp_hets(db, family):
 					"--families " + family + " " + db + " " + filter
 	ch = subprocess.check_output(ch_query,shell=True).decode('utf-8')
 	ch = ch.split('\n')
-	return(ch)
+	return(ch, ch_query)
 
 def autosomal_dominant(db, family):
 	filter = " --filter \"max_aaf_all < 0.0001 AND (is_coding=1 OR is_splicing=1) \
@@ -111,8 +111,24 @@ def autosomal_dominant(db, family):
 					"--families " + family + " " + db + " " + filter
 	ad = subprocess.check_output(ad_query,shell=True).decode('utf-8')
 	ad = ad.split('\n')
-	return(ad)
+	return(ad, ad_query)
 
+def overview(db, queries):
+	# summary stats, queries used, ped file
+	stats_query = "gemini stats --gts-by-sample " + db
+	stats = subprocess.check_output(stats_query,shell=True).decode('utf-8')
+	stats = stats.split('\n')
+	ped_query = "gemini query --header -q \"SELECT * FROM samples\" " + db
+	ped = subprocess.check_output(ped_query,shell=True).decode('utf-8')
+	ped = ped.split('\n')
+	output = []
+	output.append('Overall genotypes by sample')
+	output.extend(stats)
+	output.append('PED information used for calls')
+	output.extend(ped)
+	output.append('Gemini Queries')
+	output.extend(queries)
+	return(output)
 
 def output_to_xlsx(data,sheet_name):
 	worksheet = workbook.add_worksheet(sheet_name)
@@ -139,21 +155,30 @@ def main():
 	else:
 		family = '-'
 	# output time
-	ar = autosomal_recessive(db, family)
+	ar, ar_query = autosomal_recessive(db, family)
 	output_to_xlsx(ar, "Autosomal Recessive")	
 
-	dn = de_novo(db, family)
+	dn, dn_query = de_novo(db, family)
 	output_to_xlsx(dn, "De Novo")	
 	
-	me = mendel_errors(db, family)
+	me, me_query = mendel_errors(db, family)
 	output_to_xlsx(me, "Mendelian Errors")
 
-	ch = comp_hets(db, family)
+	ch, ch_query = comp_hets(db, family)
 	output_to_xlsx(ch, "Compound Hets")
 
-	ad = autosomal_dominant(db, family)
+	ad, ad_query = autosomal_dominant(db, family)
 	output_to_xlsx(ad, "Autosomal Dominant")
 
+	# get all queries in one list
+	queries = []
+	queries.append(ar_query.replace('\t',' ')), queries.append(dn_query.replace('\t',' '))
+	queries.append(me_query.replace('\t',' ')), queries.append(ch_query.replace('\t',' '))
+	queries.append(ad_query.replace('\t',' '))
+
+	# Create the info worksheet
+	overview_info = overview(db, queries)
+	output_to_xlsx(overview_info, "Info")
 	workbook.close()
 	
 
@@ -164,7 +189,7 @@ workbook = xlsxwriter.Workbook(args.output_name)
 columns = 	" --columns \"chrom, start, end, codon_change, aa_change, type, impact, \
 			impact_severity, gene, vep_hgvsp, aaf_1kg_all, aaf_exac_all, \
 			exac_num_hom_alt, exac_num_het, gerp_bp_score, polyphen_score, \
-			cadd_raw, sift_pred, sift_score, vep_grantham, (gt_depths).(*) \" "
+			cadd_scaled, sift_pred, sift_score, vep_grantham, (gt_depths).(*) \" "
 
 # run it!
 main()

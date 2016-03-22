6a407c70ee5d7971aa45c5aa528c8c957c66506b
David McGaughey <mcgaughey@gmail.com>
2016-03-22 12:20:00 -0400

    Added more known, reworked to exactly match GATK 3.5 recommended workflow

--- exome_workflow_v01/run_GATK_callGVCFs.sh
+++ exome_workflow_v01/run_GATK_callGVCFs.sh
@@ -3,48 +3,64 @@
 module load GATK/3.5-0
 
 input_bam=$1
-exome_interval=$2
-exome_bait_bed=$3
+exome_bait_bed=$2
 
 # Takes ~ 90 minutes
 GATK -m 8g RealignerTargetCreator \
 	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
 	-I $input_bam \
-	-knownSites /fdb/GATK_resource_bundle/hg19-2.8/1000G_phase1.indels.hg19.vcf.gz \
-	-knownSites /fdb/GATK_resource_bundle/hg19-2.8/Mills_and_1000G_gold_standard.indels.hg19.vcf.gz \
+	--known /fdb/GATK_resource_bundle/hg19-2.8/1000G_phase1.indels.hg19.vcf.gz \
+	--known /fdb/GATK_resource_bundle/hg19-2.8/Mills_and_1000G_gold_standard.indels.hg19.vcf.gz \
 	-o ${input_bam%.bam}.forIndexRealigner.intervals \
-	-L /data/mcgaugheyd/genomes/hg19/$3 \
+	-L /data/mcgaugheyd/genomes/hg19/$2 \
 	--interval_padding 100
 
 # Takes ~ 100 minutes
 GATK -m 8g IndelRealigner \
 	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
 	-I $input_bam \
-	-knownSites /fdb/GATK_resource_bundle/hg19-2.8/1000G_phase1.indels.hg19.vcf.gz \
-	-knownSites /fdb/GATK_resource_bundle/hg19-2.8/Mills_and_1000G_gold_standard.indels.hg19.vcf.gz \
+	--known /fdb/GATK_resource_bundle/hg19-2.8/1000G_phase1.indels.hg19.vcf.gz \
+	--known /fdb/GATK_resource_bundle/hg19-2.8/Mills_and_1000G_gold_standard.indels.hg19.vcf.gz \
 	-targetIntervals ${input_bam%.bam}.forIndexRealigner.intervals \
 	-o ${input_bam%.bam}.realigned.bam
 
- 
 GATK -m 8g BaseRecalibrator \
 	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
 	-I ${input_bam%.bam}.realigned.bam \
-	-knownSites /fdb/GATK_resource_bundle/hg19-2.8/dbsnp_138.hg19.excluding_sites_after_129.vcf.gz \
-	-knownSites /fdb/GATK_resource_bundle/hg19-2.8/1000G_phase1.indels.hg19.vcf.gz \
-	-knownSites /fdb/GATK_resource_bundle/hg19-2.8/Mills_and_1000G_gold_standard.indels.hg19.vcf.gz \
-	-o ${input_bam%.bam}.recal_data.table
+	--known /fdb/GATK_resource_bundle/hg19-2.8/dbsnp_138.hg19.excluding_sites_after_129.vcf.gz \
+	--known /fdb/GATK_resource_bundle/hg19-2.8/1000G_phase1.indels.hg19.vcf.gz \
+	--known /fdb/GATK_resource_bundle/hg19-2.8/Mills_and_1000G_gold_standard.indels.hg19.vcf.gz \
+	-L /data/mcgaugheyd/genomes/hg19/$2 \
+	--interval_padding 100 \
+	-o ${input_bam%.bam}.recal_data.table1
+
+GATK -m 8g PrintReads \
+	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+	-I ${input_bam%.bam}.realigned.bam \
+	-BQSR ${input_bam%.bam}.recal_data.table1 \
+	-o ${input_bam%.bam}.realigned.recalibrated.bam 
+
+GATK -m 8g BaseRecalibrator \
+	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+	-I ${input_bam%.bam}.realigned.bam \
+	--known /fdb/GATK_resource_bundle/hg19-2.8/dbsnp_138.hg19.excluding_sites_after_129.vcf.gz \
+    --known /fdb/GATK_resource_bundle/hg19-2.8/1000G_phase1.indels.hg19.vcf.gz \
+    --known /fdb/GATK_resource_bundle/hg19-2.8/Mills_and_1000G_gold_standard.indels.hg19.vcf.gz \
+	-BQSR ${input_bam%.bam}.recal_data.table1 \
+ 	-o ${input_bam%.bam}.recal_data.table2
 
 GATK -m 8g AnalyzeCovariates \
 	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
-	-BQSR ${input_bam%.bam}.recal_data.table \
+	-before ${input_bam%.bam}.recal_data.table1 \
+	-after  ${input_bam%.bam}.recal_data.table2 \
 	-plots ${input_bam%.bam}.BQSRplots.pdf
 
 # Takes ~ 180 minutes
 GATK -m 8g HaplotypeCaller \
 	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
-	-I ${input_bam%.bam}.realigned.bam \
-	-L /data/mcgaugheyd/genomes/hg19/SeqCap_EZ_Exome_v3_primary.bed \
+	-I ${input_bam%.bam}.realigned.recalibrated.bam \
+	-L /data/mcgaugheyd/genomes/hg19/$2 \
 	--interval_padding 100 \
 	--emitRefConfidence GVCF \
-	-BQSR ${input_bam%.bam}.recal_data.table \
+	-BQSR ${input_bam%.bam}.recal_data.table1 \
 	-o ${input_bam%.bam}.realigned.raw.g.vcf

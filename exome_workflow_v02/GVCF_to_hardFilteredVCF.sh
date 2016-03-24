3ed131293019cc84fcefc8f46853b25ebf89c251
David McGaughey <mcgaughey@gmail.com>
2016-03-24 10:55:15 -0400

    Updated genome ref to 1000g phaseII b37

--- exome_workflow_v02/GVCF_to_hardFilteredVCF.sh
+++ exome_workflow_v02/GVCF_to_hardFilteredVCF.sh
@@ -8,14 +8,14 @@ ped=$3
 exome_bait_bed=$4
 # Merges all GVCFs into a VCF
 GATK -m 20g GenotypeGVCFs \
-	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+	-R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37.fasta \
 	-o $2 \
 	-V $gvcfs_list \
 	--pedigree $ped
 
 # Extracts all SNPs
 GATK -m 20g SelectVariants \
-	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+	-R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37.fasta \
 	-V $2 \
 	-L $4 \
     --interval_padding 100 \
@@ -24,7 +24,7 @@ GATK -m 20g SelectVariants \
 
 # Extracts all INDELS
 GATK -m 20g SelectVariants \
-	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+	-R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37.fasta \
 	-V $2 \
 	-L $4 \
 	--interval_padding 100 \
@@ -33,7 +33,7 @@ GATK -m 20g SelectVariants \
 
 # Hard filters on GATK best practices for SNPs with MQ mod from bcbio
 GATK -m 20g VariantFiltration \
-	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+	-R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37.fasta \
 	-V ${2%.vcf}.rawSNP.vcf \
 	--filterExpression "QD < 2.0 || FS > 60.0 || MQ < 30.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
 	--filterName "FAIL_McGaughey_SNP_filter_v01" \
@@ -41,14 +41,14 @@ GATK -m 20g VariantFiltration \
 
 # Hard filters on GATK bests for INDELs
 GATK -m 20g VariantFiltration \
-	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+	-R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37.fasta \
 	-V ${2%.vcf}.rawINDEL.vcf \
 	--filterExpression "QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0" \
 	--filterName "FAIL_McGaughey_INDEL_filter_v01" \
 	-o ${2%.vcf}.filterINDEL.vcf
 	
 GATK -m 20g CombineVariants \
-	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+	-R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37.fasta \
 	--variant ${2%.vcf}.filterSNP.vcf \
 	--variant ${2%.vcf}.filterINDEL.vcf \
 	-o ${2%.vcf}.filterSNP-INDEL.vcf \

09d37bd2d50445204b2b695c6d1ced4ffd663dbe
David McGaughey <mcgaughey@gmail.com>
2016-03-24 10:10:13 -0400

    Updated ref genome path

--- exome_workflow_v02/run_bwa-mem_hg37d5.sh
+++ exome_workflow_v02/run_bwa-mem_hg37d5.sh
@@ -9,6 +9,6 @@ rg=$3
 output_name=$4
 
 bwa mem -M -t 10 -B 4 -O 6 -E 1 -M -R $rg \
-	/data/mcgaugheyd/genomes/hs37d5_1000G/hs37d5.fa \
+	/data/mcgaugheyd/genomes/1000G_phase2_GRCh37/human_g1k_v37.fasta \
 	$1 $2 | samtools view -1 - > $4
 

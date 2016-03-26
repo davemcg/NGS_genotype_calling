bb12127bf9f429711ac8b1f014b4a635853e6785
David McGaughey <mcgaughey@gmail.com>
2016-03-26 10:03:54 -0400

    Won't copy over hap contigs and changes gl naming to upper case and adds a .1 at the end to match 1000g phaseII notation

--- convert_hg19_bed_to_b37.py
+++ convert_hg19_bed_to_b37.py
@@ -12,15 +12,28 @@ chrM 100 110 to MT 100 110
 import sys
 
 counter = 0
+output = []
 for line in open(sys.argv[1]):
 	line = line.split()
 	if line[0][0:3] != "chr":
 		counter += 1
+	# remove hap contigs
+	elif "hap" in line[0]:
+		continue
+	# rename GLxxxxx contigs to mtach 1000g phaseII notation
+	elif "gl" in line[0]:
+		line[0] = line[0].split("_")[1].upper() + ".1"
+		output.append(line)
+	# Need to hand convert chrM to MT
 	elif line[0] == "chrM":
 		line[0] = "MT"
+		output.append(line)
 	elif counter > 1:
 		print("ERROR, multiple non chr lines")
 		break
 	else:
 		line[0] = line[0][3:]
+		output.append(line)
+	
+for line in output:
 	print('\t'.join(line))

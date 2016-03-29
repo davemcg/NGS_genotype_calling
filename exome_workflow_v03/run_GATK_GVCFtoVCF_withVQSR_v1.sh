2839785ac46bc14d04b1a23adf388a0d378a82c7
David McGaughey <mcgaughey@gmail.com>
2016-03-29 14:24:21 -0400

    updated truth sets to b37

--- exome_workflow_v03/run_GATK_GVCFtoVCF_withVQSR_v1.sh
+++ exome_workflow_v03/run_GATK_GVCFtoVCF_withVQSR_v1.sh
@@ -7,19 +7,19 @@ output_vcf_name=$2
 ped=$3
 # Merges all GVCFs into a VCF
 GATK -m 8g GenotypeGVCFs \
-    -R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+    -R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37_decoy.fasta \
     -o $2 \
     -V $gvcfs_list \
     --pedigree $ped
 
 # VQSR calculation time for SNPs
 GATK -m 8g VariantRecalibrator \
-    -R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+    -R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37_decoy.fasta \
     -input $2 \
-    -resource:hapmap,known=false,training=true,truth=true,prior=15.0 /fdb/GATK_resource_bundle/hg19-2.8/hapmap_3.3.hg19.vcf.gz \
-    -resource:omni,known=false,training=true,truth=true,prior=12.0 /fdb/GATK_resource_bundle/hg19-2.8/1000G_omni2.5.hg19.vcf.gz \
-    -resource:1000G,known=false,training=true,truth=false,prior=10.0 /fdb/GATK_resource_bundle/hg19-2.8/1000G_phase1.snps.high_confidence.hg19.vcf.gz \
-    -resource:dbsnp,known=true,training=false,truth=false,prior=2.0 /fdb/GATK_resource_bundle/hg19-2.8/dbsnp_138.hg19.excluding_sites_after_129.vcf.gz  \
+    -resource:hapmap,known=false,training=true,truth=true,prior=15.0 /fdb/GATK_resource_bundle/b37-2.8/hapmap_3.3.b37.vcf \
+    -resource:omni,known=false,training=true,truth=true,prior=12.0 /fdb/GATK_resource_bundle/b37-2.8/1000G_omni2.5.b37.vcf \
+    -resource:1000G,known=false,training=true,truth=false,prior=10.0 /fdb/GATK_resource_bundle/b37-2.8/1000G_phase1.snps.high_confidence.b37.vcf \
+    -resource:dbsnp,known=true,training=false,truth=false,prior=2.0 /fdb/GATK_resource_bundle/b37-2.8/dbsnp_138.b37.excluding_sites_after_129.vcf  \
     -an DP \
     -an QD \
     -an FS \
@@ -38,19 +38,19 @@ GATK -m 8g VariantRecalibrator \
 # GATK recommends using tranche of 99.9, bcbio recommend 99.97
 # http://bcb.io/2014/05/12/wgs-trio-variant-evaluation/
 GATK -m 8g ApplyRecalibration \
-    -R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+    -R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37_decoy.fasta \
     -input $2 \
     -mode SNP \
     --ts_filter_level 99.97 \
     -recalFile ${2%.vcf}.recalibrate_SNP.recal \
     -tranchesFile ${2%.vcf}.recalibrate_SNP.tranches \
-    -o ${2%.vcf}.recalibrated_snps_raw_indels.vcf 
+    -o ${2%.vcf}.recalibrated_snps_raw_indels.vcf.gz
 
 # VQSR calculation time for INDELS
 GATK -m 8g VariantRecalibrator \
-	-R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+	-R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37_decoy.fasta \
 	-input $2 \
-	-resource:mills,known=true,training=true,truth=true,prior=12.0 /fdb/GATK_resource_bundle/hg19-2.8/Mills_and_1000G_gold_standard.indels.hg19.vcf.gz \
+	-resource:mills,known=true,training=true,truth=true,prior=12.0 /fdb/GATK_resource_bundle/b37-2.8/Mills_and_1000G_gold_standard.indels.b37.vcf \
     -an QD \
     -an DP \
     -an FS \
@@ -69,12 +69,12 @@ GATK -m 8g VariantRecalibrator \
 # GATK recommends using tranche of 99.0, bcbio recommend 98.0
 # http://bcb.io/2014/05/12/wgs-trio-variant-evaluation/
 GATK -m 8g ApplyRecalibration \
-    -R /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta \
+    -R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37_decoy.fasta \
     -input ${2%.vcf}.recalibrated_snps_raw_indels.vcf \
     -mode INDEL \
     --ts_filter_level 98.0 \
     -recalFile ${2%.vcf}.recalibrate_INDEL.recal \
     -tranchesFile ${2%.vcf}.recalibrate_INDEL.tranches \
-    -o ${2%.vcf}.VQSR_recalibrated_variants.vcf
+    -o ${2%.vcf}.VQSR_recalibrated_variants.vcf.gz
 
-rm ${2%.vcf}.recalibrated_snps_raw_indels.vcf
+rm ${2%.vcf}.recalibrated_snps_raw_indels.vcf.gz

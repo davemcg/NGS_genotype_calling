654afe5fee1e4f04cb2eb409e4c35b26f2901f53
David McGaughey <mcgaughey@gmail.com>
2016-04-01 13:09:44 -0400

    fixed typos

--- exome_workflow_v02/GVCF_to_hardFilteredVCF.sh
+++ exome_workflow_v02/GVCF_to_hardFilteredVCF.sh
@@ -20,7 +20,7 @@ GATK -m 20g SelectVariants \
 	-L $4 \
     --interval_padding 100 \
 	-selectType SNP \
-	-o ${2%.vcf}.rawSNP.vcf.gz
+	-o ${2%.vcf.gz}.rawSNP.vcf.gz
 
 # Extracts all INDELS
 GATK -m 20g SelectVariants \
@@ -29,27 +29,27 @@ GATK -m 20g SelectVariants \
 	-L $4 \
 	--interval_padding 100 \
 	-selectType INDEL \
-	-o ${2%.vcf}.rawINDEL.vcf.gz
+	-o ${2%.vcf.gz}.rawINDEL.vcf.gz
 
 # Hard filters on GATK best practices for SNPs with MQ mod from bcbio
 GATK -m 20g VariantFiltration \
 	-R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37_decoy.fasta \
-	-V ${2%.vcf}.rawSNP.vcf.gz \
+	-V ${2%.vcf.gz}.rawSNP.vcf.gz \
 	--filterExpression "QD < 2.0 || FS > 60.0 || MQ < 30.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
 	--filterName "FAIL_McGaughey_SNP_filter_v01" \
-	-o ${2%.vcf}.hardFilterSNP.vcf.gz
+	-o ${2%.vcf.gz}.hardFilterSNP.vcf.gz
 
 # Hard filters on GATK bests for INDELs
 GATK -m 20g VariantFiltration \
 	-R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37_decoy.fasta \
-	-V ${2%.vcf}.rawINDEL.vcf.gz \
+	-V ${2%.vcf.gz}.rawINDEL.vcf.gz \
 	--filterExpression "QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0" \
 	--filterName "FAIL_McGaughey_INDEL_filter_v01" \
-	-o ${2%.vcf}.hardFilterINDEL.vcf.gz
+	-o ${2%.vcf.gz}.hardFilterINDEL.vcf.gz
 	
 GATK -m 20g CombineVariants \
 	-R /fdb/GATK_resource_bundle/b37-2.8/human_g1k_v37_decoy.fasta \
-	--variant ${2%.vcf}.hardFilterSNP.vcf.gz \
-	--variant ${2%.vcf}.hardFilterINDEL.vcf.gz \
-	-o ${2%.vcf}.hardFilterSNP-INDEL.vcf.gz \
+	--variant ${2%.vcf.gz}.hardFilterSNP.vcf.gz \
+	--variant ${2%.vcf.gz}.hardFilterINDEL.vcf.gz \
+	-o ${2%.vcf.gz}.hardFilterSNP-INDEL.vcf.gz \
 	--genotypemergeoption UNSORTED

88ab58f77727474c38c4d306b3f3842fbef57db9
David McGaughey <mcgaughey@gmail.com>
2016-04-04 15:56:54 -0400

    fixed logic

--- picard_samToFastq.sh
+++ picard_samToFastq.sh
@@ -2,7 +2,8 @@
 
 module load picard
 
+mkdir ${1%.bam}_fastq
 java -Xmx20g -jar -jar $PICARDJARPATH/picard.jar SamToFastq \
 	I=$1 \
-	FASTQ=${1%.bam}_1.fastq \
-	SECOND_END_FASTQ=${1%.bam}_2.fastq
+	OUTPUT_PER_RG="true" \
+	OUTPUT_DIR=${1%.bam}_fastq

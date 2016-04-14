5522a61a3a8e8bd3ebb21d7fce8bd96aee7be686
David McGaughey <mcgaughey@gmail.com>
2016-04-14 15:22:36 -0400

    Kind of takes input from stdin. Needs a bit of tweaking to split on \n

--- split_matrix_by_col.py
+++ split_matrix_by_col.py
@@ -2,9 +2,11 @@
 
 import argparse
 import os.path
+import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('file', help = 
+parser.add_argument('file', nargs='?',\
+	help = 
 	"Takes in a file (intended for use for the NISC lane bam \
 	exome to GVCF pipeline matrix) and splits it into separate \
 	files by the first column. \
@@ -23,14 +25,22 @@ parser.add_argument('file', help =
 		Sample2 131129_YOSHI_C2P4RACXX.1.548979")
 
 args = parser.parse_args()
-file = args.file
-
-for line in open(file):
+# if file given
+if args.file:
+	file = open(args.file)
+#if no file given, then use stdin 
+else:
+	file = []
+	file.append(sys.stdin.read())
+	
+for line in file:
 	s_line = line.split()
 	sample = s_line[0]
 	sample_file_name = sample + ".laneBam.matrix"
+	# if file doesn't exist, then create it and write the line
 	if not os.path.isfile(sample_file_name):
 		sample_file = open(sample_file_name, 'w')
 		sample_file.write(line)
+	# if file already exists, then just write line
 	else:
 		sample_file.write(line)

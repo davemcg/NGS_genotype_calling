55a024c3693e4a4571f6c7a3d354ed59305287da
David McGaughey <mcgaughey@gmail.com>
2016-04-28 14:28:33 -0400

    fixed paths

--- casey_workflow_v01/align_with_bwa.py
+++ casey_workflow_v01/align_with_bwa.py
@@ -3,9 +3,8 @@
 import argparse
 import subprocess
 
-parser = argparse.ArgumentParser()
-parser.add_argument('file', help= 
-	"Takes in fastq.gz files from Casey panels,
+parser = argparse.ArgumentParser(description= \
+	"Takes in fastq.gz files from Casey panels, \
 	aligns with bwa-mem to 1000g phaseII hs37d5 with \
 	read group info  \
 	Requires samtools/1.3 and bwa to be loaded \
@@ -14,16 +13,22 @@ parser.add_argument('file', help=
 	\
 	Example (exome): \
    		sbatch --mem=30G --cpus-per-task 10 this_script.py A_BAM_FILE_001.bam")
+parser.add_argument('forward', help = 'Forward fastq reads')
+parser.add_argument('reverse', help = 'Reverse fastq reads')
+parser.add_argument('CaseyID', help = 'Casey sample ID, e.g. 15-00414')
+parser.add_argument('panel', help = 'Casey Panel')
 args = parser.parse_args()
-bamfile = args.file
-bam_name = bamfile.split('.bam')[0]
+forward = args.forward
+reverse = args.reverse
+caseyID = args.CaseyID
+panel = args.panel
 
 # ID is basically the NISC file name for the fastq (minus the .fq at the end)
-ID = 'ID:' + fastq_file[0:-3]
+ID = 'ID:' + forward.split('.fastq')[0]
 # SM is the bam file name or sample name. Needs to the same for each sample!
-SM = 'SM:' + bam_name
+SM = 'SM:' + caseyID + '_' + panel
 # LB is the library
-LB = 'LB:' + fastq_file.split('.')[2]
+LB = 'LB:' + forward.split('.fastq')[0]
 PL = 'PL:Illumina\\" \\'
 
 Output = SM + '.bwa-mem.b37.bam'
@@ -34,9 +39,9 @@ RG_core = '\\\\t'.join(['\\"\@RG',ID, SM, LB, PL])
 # bwa alignment
 print("BWA run beginning")
 run_bwa =   ('/home/mcgaugheyd/bin/exome_workflow_v02/run_bwa-mem_hg37d5.sh ' +
-            bam_name + '_1.fastq.gz ' + bam_name + '_2.fastq.gz ' +
+            forward + ' ' + reverse + ' ' + 
             '\\@RG\\\\t' + ID + '\\\\t' + SM + '\\\\t' + LB + '\\\\t' + 'PL:Illumina ' +
-            bam_name + '.bwa-mem.b37.bam')
+            caseyID + '/' + caseyID + '_' + panel  + '.bwa-mem.b37.bam')
 print(run_bwa)
 subprocess.check_call(run_bwa, shell=True)
 print("All done!")

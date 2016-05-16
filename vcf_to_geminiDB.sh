f56777a8540606a46fe53eb76d2fe97a59b7e673
David McGaughey <mcgaughey@gmail.com>
2016-05-16 12:31:06 -0400

    Tiny script to automate vcf to gemini DB creation

---
+++ vcf_to_geminiDB.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Assumes a GATK processed VCF after GATK-recommended filtering
+# Hard coded against grch37
+module load vcftools
+module load gemini/0.18
+
+VCF=$1
+PED=$2
+DBNAME=$3
+
+mkdir tmp
+
+#only keep AF > 0.25
+#vcffilter -f "AF > 0.25" $VCF > tmp/$VCF.AFfiltered
+
+# vt to "left align and trim alternative variants"
+cat $VCF \
+	| sed 's/ID=AD,Number=./ID=AD,Number=R/' \
+	| ~/git/vt/./vt decompose -s - \
+	| ~/git/vt/./vt normalize -r /data/mcgaugheyd/genomes/1000G_phase2_GRCh37/human_g1k_v37_decoy.fasta - \
+	> tmp/$VCF 
+
+# annotate with VEP
+/home/mcgaugheyd/bin/run_VEP.sh tmp/$VCF GRCh37 $SLURM_CPUS_PER_TASK
+
+# move out of tmp folder
+mv tmp/${VCF%.vcf}.VEP.GRCh37.vcf* . 
+
+# compress and index
+bgzip ${VCF%.vcf}.VEP.GRCh37.vcf
+tabix -p vcf ${VCF%.vcf}.VEP.GRCh37.vcf.gz
+
+rm -rf tmp
+
+
+######## Now create gemini DB #############
+gemini load --cores $SLURM_CPUS_PER_TASK -t VEP -v ${VCF%.vcf}.VEP.GRCh37.vcf.gz -p $PED $DBNAME

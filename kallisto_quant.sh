7dcadf4fc63a642c66a5442f8582ddd1dfab9b0c
David McGaughey <mcgaughey@gmail.com>
2016-06-24 12:39:01 -0400

    can call mouse or human on the first input. reads on 2nd and 3rd inputs

--- kallisto_quant.sh
+++ kallisto_quant.sh
@@ -2,8 +2,28 @@
 
 module load kallisto/0.42.4
 
-kallisto quant -i /data/mcgaugheyd/genomes/GRCh38/0.42.4_Homo_sapiens.GRCh38.rel84.cdna.all.idx \
-			   -o ${1%_*}_kallisto \
-               -b 100 \
-               -t $SLURM_CPUS_PER_TASK \
-			   $1 $2
+# indexes
+# /data/mcgaugheyd/genomes/GRCm38/kallisto_index/gencode.vM8.idx 
+# /data/mcgaugheyd/genomes/GRCh38/0.42.4_Homo_sapiens.GRCh38.rel84.cdna.all.idx
+
+index=$1
+index=$(echo $index | tr 'a-z' 'A-Z')
+read_1=$2
+read_2=$3
+
+if [ $index == 'HUMAN' ]; then
+		kallisto quant -i /data/mcgaugheyd/genomes/GRCh38/0.42.4_Homo_sapiens.GRCh38.rel84.cdna.all.idx \
+		   -o ${2%_*}_kallisto \
+           -b 100 \
+           -t $SLURM_CPUS_PER_TASK \
+		   $read_1 $read_2 
+elif [ $index == 'MOUSE' ]; then
+		kallisto quant -i /data/mcgaugheyd/genomes/GRCm38/kallisto_index/gencode.vM8.idx \
+    	   -o ${2%_*}_kallisto \
+           -b 100 \
+           -t $SLURM_CPUS_PER_TASK \
+           $read_1 $read_2 
+else
+	echo "Give either Mouse or Human"
+fi
+

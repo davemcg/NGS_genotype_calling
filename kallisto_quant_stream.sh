31f91a543ae1f1e4b69549943fe9c2e26e00339b
David McGaughey <mcgaughey@gmail.com>
2016-06-27 14:32:35 -0400

    cleaner, more general for url streaming by directly calling curl

--- kallisto_quant_stream.sh
+++ kallisto_quant_stream.sh
@@ -5,9 +5,10 @@ module load kallisto/0.42.4
 # will stream to kallisto, not saving fastq
 # ftp.sra.ebi.ac.uk/vol1/fastq/ERR030/ERR030887/ERR030887_1.fastq.gz
 output_folder=$1
+fastq_url=$2
 kallisto quant -i /data/mcgaugheyd/genomes/GRCh38/0.42.4_Homo_sapiens.GRCh38.rel84.cdna.all.idx \
 			   -o $1 \
                -b 100 \
                -t $SLURM_CPUS_PER_TASK \
-			   <(~/bin/stream_fastq_ftp.sh $2) \
-			   <(~/bin/stream_fastq_ftp.sh ${2%_1.fastq.gz}_2.fastq.gz)
+			   <(curl -s $fastq_url | zcat ) \
+			   <(curl -s ${fastq_url%_1.fastq.gz}_2.fastq.gz | zcat )

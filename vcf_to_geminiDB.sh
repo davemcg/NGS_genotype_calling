6fda2b8cbb176554a0e0cbbc6b79d5c69eada6c2
David McGaughey <mcgaughey@gmail.com>
2016-08-09 15:07:40 -0400

    fixed code I commented out

--- vcf_to_geminiDB.sh
+++ vcf_to_geminiDB.sh
@@ -9,23 +9,23 @@ VCF=$1
 PED=$2
 DBNAME=$3
 
-#mkdir tmp
+mkdir tmp
 
 #only keep AF > 0.25
 #vcffilter -f "AF > 0.25" $VCF > tmp/$VCF.AFfiltered
 
-# vt to "left align and trim alternative variants"
-#cat $VCF \
-#	| sed 's/ID=AD,Number=./ID=AD,Number=R/' \
-#	| ~/git/vt/./vt decompose -s - \
-#	| ~/git/vt/./vt normalize -r /data/mcgaugheyd/genomes/1000G_phase2_GRCh37/human_g1k_v37_decoy.fasta - \
-#	> tmp/${VCF%.gz}
+#vt to "left align and trim alternative variants"
+cat $VCF \
+	| sed 's/ID=AD,Number=./ID=AD,Number=R/' \
+	| ~/git/vt/./vt decompose -s - \
+	| ~/git/vt/./vt normalize -r /data/mcgaugheyd/genomes/1000G_phase2_GRCh37/human_g1k_v37_decoy.fasta - \
+	> tmp/${VCF%.gz}
 
 # annotate with VEP
-#/home/mcgaugheyd/bin/run_VEP.sh tmp/${VCF%.gz} GRCh37 $SLURM_CPUS_PER_TASK
+/home/mcgaugheyd/bin/run_VEP.sh tmp/${VCF%.gz} GRCh37 $SLURM_CPUS_PER_TASK
 
 # move out of tmp folder
-#mv tmp/${VCF%.vcf.gz}.VEP.GRCh37.vcf* . 
+mv tmp/${VCF%.vcf.gz}.VEP.GRCh37.vcf* . 
 
 # compress and index
 bgzip ${VCF%.vcf.gz}.VEP.GRCh37.vcf

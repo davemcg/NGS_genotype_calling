c40cf71823bf923927a444af9a3a291a7af495b8
David McGaughey <mcgaughey@gmail.com>
2016-12-22 14:42:08 -0500

    streams out changes instead of saving to temp file

--- convert_hg19_bed_to_b37.py
+++ convert_hg19_bed_to_b37.py
@@ -23,17 +23,14 @@ for line in open(sys.argv[1]):
 	# rename GLxxxxx contigs to mtach 1000g phaseII notation
 	elif "gl" in line[0]:
 		line[0] = line[0].split("_")[1].upper() + ".1"
-		output.append(line)
+		print('\t'.join(line))
 	# Need to hand convert chrM to MT
 	elif line[0] == "chrM":
 		line[0] = "MT"
-		output.append(line)
+		print('\t'.join(line))
 	elif counter > 1:
 		print("ERROR, multiple non chr lines")
 		break
 	else:
 		line[0] = line[0][3:]
-		output.append(line)
-	
-for line in output:
-	print('\t'.join(line))
+		print('\t'.join(line))	

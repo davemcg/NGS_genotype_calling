33c046a6b68ba820d6ea5a79687747d507a1d1d4
David McGaughey <mcgaughey@gmail.com>
2017-04-26 21:37:48 -0400

    bwa mem wrapper starting with a generic bam

---
+++ exome_workflow_v02/run_bwa-mem_hg37d5_fromBam.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module load bwa/0.7.12
+module load samtools/1.4
+
+input_bam=$1
+rg=$2
+output_name=$3
+
+samtools collate -uOn 128 $input_bam /tmp/TMP-$input_bam | samtools fastq - | bwa mem -M -t 10 -B 4 -O 6 -E 1 -M -p -R $rg /data/mcgaugheyd/genomes/1000G_phase2_GRCh37/human_g1k_v37_decoy.fasta - | samtools view -1 - > $output_name
+

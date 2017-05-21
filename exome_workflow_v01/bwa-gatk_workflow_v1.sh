9a3334b83f9881b0e8610b46401b3dd834a59e81
David McGaughey <mcgaughey@gmail.com>
2017-05-20 20:52:40 -0400

    updating paths

--- exome_workflow_v01/bwa-gatk_workflow_v1.sh
+++ exome_workflow_v01/bwa-gatk_workflow_v1.sh
@@ -16,18 +16,18 @@ nisc_input_bam=$1
 ############
 #RE-ALIGNMENT
 # pulls bwa-formatted info (read group info, bam file, etc) then hands over to sbatch 
-j1=$(sbatch --job-name bwa.$1 --mem=30g --cpus-per-task 10 ~/bin/exome_workflow_v01/realign_NISC_bam_with_bwa.py $1)
+j1=$(sbatch --job-name bwa.$1 --mem=30g --cpus-per-task 10 ~/git/NGS_genotype_calling/exome_workflow_v01/realign_NISC_bam_with_bwa.py $1)
 ############
 
 ############
 # BAM processing
 # sort, mark duplicates, and index bam
-j2=$(sbatch --job-name sortMarkIndex.$1 --dependency=afterok:$j1 --mem=20G ~/bin/exome_workflow_v01/run_Picard_SortMarkIndexBam.sh ${1%.bam}.bwa-mem.hg19.bam)
+j2=$(sbatch --job-name sortMarkIndex.$1 --dependency=afterok:$j1 --mem=20G ~/git/NGS_genotype_calling/exome_workflow_v01/run_Picard_SortMarkIndexBam.sh ${1%.bam}.bwa-mem.hg19.bam)
 ############
 
 ############
 # Call GVCF
 # Individual GVCF file, which in conjuction with other GVCFs and filtering (hard or VQSR) are used
 # to make a VCF file with called genotypes
-j3=$(sbatch --job-name callGVCF.$1 --dependency=afterok:$j2 --mem=16g --time=16:00:00 ~/bin/exome_workflow_v01/run_GATK_callGVCFs.sh ${1%.bam}.bwa-mem.hg19.sorted.markDup.bam)
+j3=$(sbatch --job-name callGVCF.$1 --dependency=afterok:$j2 --mem=16g --time=16:00:00 ~/git/NGS_genotype_calling/exome_workflow_v01/run_GATK_callGVCFs.sh ${1%.bam}.bwa-mem.hg19.sorted.markDup.bam)
 ############

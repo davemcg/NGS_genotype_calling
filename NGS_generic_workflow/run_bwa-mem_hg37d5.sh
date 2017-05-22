4012ac2eaab5e0de29eb1147fb334df5ee3cfd7c
David McGaughey <mcgaughey@gmail.com>
2017-05-22 15:22:37 -0400

    bwa threads used taken from SLURM

--- NGS_generic_workflow/run_bwa-mem_hg37d5.sh
+++ NGS_generic_workflow/run_bwa-mem_hg37d5.sh
@@ -8,7 +8,7 @@ fastq2=$2
 rg=$3
 output_name=$4
 
-bwa mem -M -t 10 -B 4 -O 6 -E 1 -M -R $rg \
+bwa mem -M -t $SLURM_CPUS_PER_TASK -B 4 -O 6 -E 1 -M -R $rg \
 	/data/mcgaugheyd/genomes/1000G_phase2_GRCh37/human_g1k_v37_decoy.fasta \
 	$1 $2 | samtools view -1 - > $4
 
--- NGS_generic_workflow/run_bwa-mem_hg37d5_fromBam.sh
+++ NGS_generic_workflow/run_bwa-mem_hg37d5_fromBam.sh
@@ -9,6 +9,6 @@ output_name=$3
 
 samtools collate -uOn 128 $input_bam /tmp/TMP-$input_bam | \
 	samtools fastq - | \
-	bwa mem -M -t 10 -B 4 -O 6 -E 1 -M -p -R $rg /data/mcgaugheyd/genomes/1000G_phase2_GRCh37/human_g1k_v37_decoy.fasta - | \
+	bwa mem -M -t $SLURM_CPUS_PER_TASK -B 4 -O 6 -E 1 -M -p -R $rg /data/mcgaugheyd/genomes/1000G_phase2_GRCh37/human_g1k_v37_decoy.fasta - | \
 	samtools view -1 - > $output_name
 

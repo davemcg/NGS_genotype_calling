2a328293ba468760850f2857659f8229644a1249
Bin-Guan <binguan2@gmail.com>
2020-03-16 09:25:33 -0400

    v1 bcl2fastq

---
+++ bcl2fastq.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#sinteractive --cpus-per-task=20 --mem=128g
+#finished one midi-run in less than 5 min.
+#samplesheet should be named as SampleSheet.csv if no sample sheet is specified in the command below
+
+module load bcl2fastq/2.20.0
+
+bcl2fastq --runfolder-dir . --output-dir ./bcl2fastq -r 4 -w 4 -p 12 --barcode-mismatches 0
+
+#read 4, write 4, processing 12
+
+

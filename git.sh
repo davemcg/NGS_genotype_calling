c30f5809fc02d036f6a3105b7d51699dae277c6b
Bin-Guan <binguan2@gmail.com>
2024-01-25 23:33:08 -0500

    ampSeq and other dev tools

--- git.sh
+++ git.sh
@@ -4,7 +4,7 @@
 echo $1 "$2"
 git add --all
 git commit -a -m "$2"
-git tag -a $1 -m "$2"
+git tag --force -a $1 -m "$2"
 git log | head -n 5 > /data/OGL/resources/NGS_genotype_calling.git.log
 git tag | tail -n 1 >> /data/OGL/resources/NGS_genotype_calling.git.log
 git push origin $1
